e8d3005d07c61e81afebe1f0eb1f58e0ac434d7b
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-24 21:00:47 +0000

    >> added spellcheck > added comments to lua/colon.lua

--- lua/colon.lua
+++ lua/colon.lua
@@ -1,5 +1,6 @@
 -- module for any standalone user-defined commands that I make
 
+-- system-open the current buffer (probably in a browser)
 vim.api.nvim_create_user_command(
 	"Open",
 	function()
@@ -7,3 +8,19 @@ vim.api.nvim_create_user_command(
 	end,
 	{}
 )
+
+
+-- spellcheck
+vim.api.nvim_create_user_command("Spell",
+	function ()
+		vim.cmd.setlocal("spell spelllang=en_gb")
+	end,
+	{}
+)
+vim.api.nvim_create_user_command("Nospell",
+	function ()
+		vim.cmd.setlocal("nospell")
+	end,
+	{}
+)
+
