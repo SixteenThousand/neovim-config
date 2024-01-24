13110df450fe8157d864d1111cdf17103db13bfc
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-24 23:39:45 +0000

    >> made utils return stuff

--- lua/terminal/utils.lua
+++ lua/terminal/utils.lua
@@ -1,4 +1,5 @@
 -- this is mainly here to hold the "make terminal window" function
+local M = {}
 
 -- creates a new window to the right of the current one, and starts a terminal
 -- in that window with a command, if given, and with the currently working 
@@ -10,7 +11,7 @@
 -- 	terminal
 -- focus: boolean; If true, moves the cursor to the new window. Otherwise
 -- 	cursor is kept in current window.
-function terminal_vsplit(cmd,focus)
+function M.terminal_vsplit(cmd,focus)
 	-- first check whether there is already a window to the right of the 
 	-- current one
 	local winid = vim.fn.win_getid()
@@ -37,3 +38,6 @@ function terminal_vsplit(cmd,focus)
 		vim.cmd.wincmd("h")
 	end
 end
+
+
+return M
