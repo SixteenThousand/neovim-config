1081042fb36cacd5c4e6fa85e17040ce91f12002
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-25 00:37:00 +0000

    >> wrote yona connections

--- lua/terminal/yona.lua
+++ lua/terminal/yona.lua
@@ -1,1 +1,45 @@
 -- time to write a new version of yona???
+
+local utils = require("terminal.utils")
+
+vim.api.nvim_create_user_command(
+	"Yona",
+	function(opts)
+		local yona = "!cd "..vim.fn.expand("%:h").." && yona "..opts.fargs[1]
+		if opts.bang then
+			utils.terminal_vsplit(yona)
+		else
+			vim.cmd(yona)
+		end
+	end,
+	{nargs=1}
+)
+vim.keymap.set({"n","i"},"<A-i>",":Yona build")
+vim.keymap.set({"n","i"},"<A-S-i>",":Yona! build")
+
+
+vim.keymap.set({"n","i"},"<A-o>",function()
+	local path = vim.fn.expand("%:h")
+	local name,_ = vim.fn.expand("%:t"):gsub("%..*$","")
+	local ext = vim.fn.expand("%:e")
+	vim.cmd("!yona compile -p "..path.." -n "..name.." -e "..ext)
+end)
+vim.keymap.set({"n","i"},"<A-S-o>",function()
+	local path = vim.fn.expand("%:h")
+	local name,_ = vim.fn.expand("%:t"):gsub("%..*$","")
+	local ext = vim.fn.expand("%:e")
+	utils.terminal_vsplit("!yona compile -p "..path.." -n "..name.." -e "..ext)
+end)
+
+vim.keymap.set({"n","i"},"<A-p>",function()
+	local path = vim.fn.expand("%:h")
+	local name,_ = vim.fn.expand("%:t"):gsub("%..*$","")
+	local ext = vim.fn.expand("%:e")
+	vim.cmd("yona run -p "..path.." -n "..name.." -e "..ext)
+end)
+vim.keymap.set({"n","i"},"<A-S-p>",function()
+	local path = vim.fn.expand("%:h")
+	local name,_ = vim.fn.expand("%:t"):gsub("%..*$","")
+	local ext = vim.fn.expand("%:e")
+	utils.terminal_vsplit("yona run -p "..path.." -n "..name.." -e "..ext)
+end)
