f40a76554867c269952be16c0614c321d4c25cb3
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-25 16:26:45 +0000

    >> updated init.lua > added missing imports to files > sorted the two groups of imports

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 -- loose config files
+require("colon")
 require("remap") -- this has to come before lazy is called
 require("settings")
+require("syntax")
 
-require("plugins")
-require("nav")
-require("terminal")
-require("pretty")
 require("amber")
+require("nav")
+require("plugins")
+require("pretty")
+require("terminal")
 
 
 print("And It Goes On...")
