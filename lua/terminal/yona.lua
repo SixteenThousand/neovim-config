155d0111e7032d74cec70a122c36852612b25a79
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-29 12:52:24 +0000

    >> bugfix: "compile in vsplit" functionality now works

--- lua/terminal/yona.lua
+++ lua/terminal/yona.lua
@@ -28,7 +28,7 @@ vim.keymap.set({"n","i"},"<A-S-o>",function()
 	local path = vim.fn.expand("%:h")
 	local name,_ = vim.fn.expand("%:t"):gsub("%..*$","")
 	local ext = vim.fn.expand("%:e")
-	utils.terminal_vsplit("!yona compile -p "..path.." -n "..name.." -e "..ext)
+	utils.terminal_vsplit("yona compile -p "..path.." -n "..name.." -e "..ext)
 end)
 
 vim.keymap.set({"n","i"},"<A-p>",function()
