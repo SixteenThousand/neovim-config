fc223fce173772693d15ca3e2504985119fd708d
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-03-22 17:29:01 +0000

    >> created :Salt command

--- lua/nav/init.lua
+++ lua/nav/init.lua
@@ -2,3 +2,23 @@ local fuzzyOn,fuzzy = pcall(require,"nav.fuzzy")
 require("nav.remaps")
 require("nav.folding")
 -- nav.manager is sourced from the relevant file manager`s config
+
+-- opens a view on some given buffer(specified as with :buffer) and deletes the 
+-- current buffer afterwards ("salting the earth" in its wake)
+vim.api.nvim_create_user_command(
+    "Salt",
+    function(opts)
+        -- find out where we are
+        local here = vim.fn.bufnr()
+        -- find out where we're going & go there
+        -- default case
+        if opts.fargs[1] == nil then
+            vim.cmd.edit(vim.fn.expand("%:h"))
+        else
+            vim.cmd.buffer(opts.fargs[1])
+        end
+        -- get rid of original buffer
+        vim.cmd.bdelete(here)
+    end,
+    {nargs="?"}
+)
