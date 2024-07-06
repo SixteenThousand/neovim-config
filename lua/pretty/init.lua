5be56a81c6f11fa261c0f93da82822f09a282aab
Thomas Lindop <thomas16256@gmail.com>
2024-07-06 20:31:23 +0100

    >> add error handling for loading of gui shim plugin

--- lua/pretty/init.lua
+++ lua/pretty/init.lua
@@ -2,10 +2,12 @@ require("pretty.colours")
 if vim.g.neovide then
 	require("pretty.neovide")
 else
-	vim.cmd.GuiRenderLigatures(true)
-	vim.cmd("GuiWindowOpacity 0.85")
-	vim.g.GuiMouseHide = 1
-	vim.cmd.call("GuiWindowFullScreen(1)")
-	-- vim.cmd.call("GuiWindowFrameless(1)")
-	vim.o.guifont = "FantasqueSansM Nerd Font Mono:h9"
+	local guiOn,_ = pcall(vim.cmd.GuiRenderLigatures,true)
+    if guiOn then
+        vim.cmd("GuiWindowOpacity 0.85")
+        vim.g.GuiMouseHide = 1
+        vim.cmd.call("GuiWindowFullScreen(1)")
+        -- vim.cmd.call("GuiWindowFrameless(1)")
+        vim.o.guifont = "FantasqueSansM Nerd Font Mono:h9"
+    end
 end
