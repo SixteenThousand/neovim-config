0f37e2308a995f7a3d3b74a6a491025705241aad
Thomas Lindop <thomas16256@gmail.com>
2024-07-06 16:03:22 +0100

    >> add dark mode toggle >> remove commented-out colourschemes

--- lua/pretty/colours.lua
+++ lua/pretty/colours.lua
@@ -1,9 +1,15 @@
 -- colourscheme management
 
 -- default
--- vim.cmd.colorscheme("slate")
--- vim.cmd.colorscheme("kanagawa")
-vim.cmd.colorscheme("sonokai")
+vim.cmd.colorscheme("desert")
+
+vim.keymap.set("n","<leader>cb",function()
+    if vim.o.background == "dark" then
+        vim.o.background = "light"
+    else
+        vim.o.background = "dark"
+    end
+end)
 
 local scopeOn,tlscp = pcall(require,"telescope.builtin")
 if scopeOn then
