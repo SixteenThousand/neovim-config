f8f96a41d513295aeb40ca99c2d2c9a11915dcb1
Thomas Lindop <thomas16256@gmail.com>
2024-07-17 09:23:07 +0100

    >> use a for loop to create all the <leader>e... keymaps

--- lua/nav/remaps.lua
+++ lua/nav/remaps.lua
@@ -1,35 +1,24 @@
 -- general navigation remaps; remaps for related to more specific things
 -- are in their respective modules
 vim.keymap.set("n","<leader>ee",":buffer ")
--- vim.keymap.set("n","<leader>es",":sbuffer ")
 vim.keymap.set("n","<leader>ef",":find ")
 
-vim.keymap.set("n","<leader>el",function ()
-	vim.cmd.edit("~/Projects/Learning")
-	vim.o.number = true
-end)
+local bookmarks = {
+    ["'"] = vim.fn.stdpath("config"),
+    [";"] = vim.fn.stdpath("data"),
+    ["#"] = os.getenv("XDG_CONFIG_HOME") or "~/.config",
+    ["l"] = "~/Projects/Learning",
+    ["p"] = "~/Projects",
+    ["i"] = "~/Projects/Ideas/ideas.md",
+    ["t"] = "~/temp",
+}
 
-vim.keymap.set("n","<leader>ep",function ()
-	vim.cmd.edit("~/Projects")
-	vim.o.number = true
-end)
-vim.keymap.set("n","<leader>et",function ()
-	vim.cmd.edit("~/temp")
-	vim.o.number = true
-end)
-
-vim.keymap.set("n","<leader>e'",function ()
-    vim.cmd.edit(vim.fn.stdpath("config"))
-	vim.o.number = true
-end)
-vim.keymap.set("n","<leader>e;",function ()
-    vim.cmd.edit(vim.fn.stdpath("data"))
-	vim.o.number = true
-end)
-vim.keymap.set("n","<leader>e#",function ()
-    vim.cmd.edit(os.getenv("XDG_CONFIG_HOME") or "~/.config")
-    vim.o.number = true
-end)
+for key,path in pairs(bookmarks) do
+    vim.keymap.set("n","<leader>e"..key, function ()
+        vim.cmd.edit(path)
+        vim.o.number = true
+    end)
+end
 
 
 vim.keymap.set("n","-",function ()
