94138df035f56a7fd810fc5ae953a850a23fd291
Thomas Lindop <thomas16256@gmail.com>
2024-08-02 22:37:08 +0100

    >> add more syntax highlighting to clifm config files

--- syntax/clifm.lua
+++ syntax/clifm.lua
@@ -2,3 +2,11 @@ vim.cmd.syntax("match clifmComment1 /#.*/")
 vim.cmd.syntax("match clifmComment2 /;.*/")
 vim.cmd.highlight("link clifmComment1 comment")
 vim.cmd.highlight("link clifmComment2 comment")
+
+vim.o.commentstring = ";%s"
+
+vim.cmd.syntax([[match clifmSetting /^[^;#][^=]*/]])
+vim.cmd.highlight("link clifmSetting Identifier")
+
+vim.cmd.syntax([[match clifmOperator /=/]])
+vim.cmd.highlight("link clifmOperator operator")
