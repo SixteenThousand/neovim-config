ad417d7156e0d676c4cb4646008d5f11c6e07c80
Thomas Lindop <thomas16256@gmail.com>
2024-09-01 22:06:30 +0100

    >> add java http handler snippet

--- lua/snippets/java.lua
+++ lua/snippets/java.lua
@@ -8,6 +8,28 @@ local START_SNIPPET = [[public class <> {
     }
 }]]
 
+local HTTP_HANDLER_SNIPPET = [[ import java.io.OutputStream;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+
+public class <> implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(<>,<>);
+        OutputStream response = exchange.getResponseBody();
+        <>
+        response.close();
+    }
+}]]
+
 ls.add_snippets("java",{
     s("start",fmt(START_SNIPPET,{i(1),i(2)},{delimiters="<>"})),
+    s(
+        "httphandler",
+        fmt(
+            HTTP_HANDLER_SNIPPET,
+            {i(1),i(2),i(3),i(4),i(5),},
+            {delimiters="<>"}
+        )
+    ),
 })
