4bafa57eb60a5a02cd0fa6be2410b3d16310cd62
Thomas Lindop <thomas16256@gmail.com>
2024-09-19 20:43:51 +0100

    >> update yona integration for yona v0.4

--- lua/terminal/yona.lua
+++ lua/terminal/yona.lua
@@ -18,29 +18,31 @@ vim.api.nvim_create_user_command(
 	"RipgrepYona",
 	function(opts)
 		utils.terminal_vsplit(
-			"cd "..vim.fn.expand("%:h").." && yona sh rg "..opts.fargs[1],
+			"cd "..vim.fn.expand("%:h").." && yona -s rg "..opts.fargs[1],
 			true
 		)
 	end,
 	{nargs=1}
 )
-vim.keymap.set({"n","i"},"<A-i>",":Yona build<CR>")
-vim.keymap.set({"n","i"},"<A-S-i>",":Yona! build<CR>")
-vim.keymap.set({"n","i"},"<A-u>",":Yona test<CR>")
-vim.keymap.set({"n","i"},"<A-S-u>",":Yona! test<CR>")
+vim.keymap.set({"n","i"},"<A-i>",":Yona test<CR>")
+vim.keymap.set({"n","i"},"<A-S-i>",":Yona! test<CR>")
+vim.keymap.set({"n","i"},"<A-o>",":Yona build<CR>")
+vim.keymap.set({"n","i"},"<A-S-o>",":Yona! build<CR>")
+vim.keymap.set({"n","i"},"<A-p>",":Yona run<CR>")
+vim.keymap.set({"n","i"},"<A-S-p>",":Yona! run<CR>")
 
 
-vim.keymap.set({"n","i"},"<A-o>",function()
-	vim.cmd("!yona -s compile "..vim.fn.expand("%:p"))
+vim.keymap.set({"n","i"},"<A-j>",function()
+	vim.cmd("!yona -f compile %:p")
 end)
-vim.keymap.set({"n","i"},"<A-S-o>",function()
+vim.keymap.set({"n","i"},"<A-S-j>",function()
 	local path = vim.fn.expand("%:h")
-	utils.terminal_vsplit("yona -s compile "..vim.fn.expand("%:p"))
+	utils.terminal_vsplit("yona -f compile "..vim.fn.expand("%:p"))
 end)
 
-vim.keymap.set({"n","i"},"<A-p>",function()
-	vim.cmd("!yona -s run "..vim.fn.expand("%:p"))
+vim.keymap.set({"n","i"},"<A-k>",function()
+	vim.cmd("!yona -f run %:p")
 end)
-vim.keymap.set({"n","i"},"<A-S-p>",function()
-	utils.terminal_vsplit("yona -s run "..vim.fn.expand("%:p"))
+vim.keymap.set({"n","i"},"<A-S-k>",function()
+	utils.terminal_vsplit("yona -f run "..vim.fn.expand("%:p"))
 end)
