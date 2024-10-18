f3f1ed708c8960ce7e47e200c307baec4974da3e
Thomas Lindop <thomas16256@gmail.com>
2024-10-18 16:41:44 +0100

    >> load colourscheme from file to share acrosss instances

--- lua/pretty/colours.lua
+++ lua/pretty/colours.lua
@@ -1,10 +1,19 @@
 -- colourscheme management
+local COLOUR_SCHEME_FILE = vim.fn.stdpath("config").."/state/colourscheme"
 
 -- default
 vim.o.background = "dark"
-local defaultColourSchemeOn, _ = pcall(vim.cmd.colorscheme,"everforest")
+local csFp = io.open(COLOUR_SCHEME_FILE, "r")
+local defaultCs
+if csFp then
+    defaultCs = csFp:read()
+    csFp:close()
+else
+    defaultCs = "sonokai"
+end
+local defaultColourSchemeOn, _ = pcall(vim.cmd.colorscheme,defaultCs)
 if not defaultColourSchemeOn then
-    vim.cmd.colorscheme("slate")
+    vim.cmd.colorscheme("zaibatsu")
 end
 
 vim.keymap.set("n","<leader>cb",function()
@@ -19,3 +28,13 @@ local scopeOn,tlscp = pcall(require,"telescope.builtin")
 if scopeOn then
 	vim.keymap.set("n","<leader>fc",tlscp.colorscheme)
 end
+
+vim.api.nvim_create_autocmd("ColorScheme",{
+    callback = function()
+        local csFp = io.open(COLOUR_SCHEME_FILE,"w")
+        if csFp then
+            csFp:write(vim.fn.expand("<amatch>"))
+            csFp:close()
+        end
+    end,
+})
