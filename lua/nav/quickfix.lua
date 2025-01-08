95e7c293de2bd2444a54e69205f1cdc25e4fab5a
Thomas Lindop <thomas16256@gmail.com>
2025-01-08 19:37:21 +0000

    >> centre quickfix entries when navigating the list

--- lua/nav/quickfix.lua
+++ lua/nav/quickfix.lua
@@ -4,8 +4,14 @@ local fix_quick = function()
 end
 
 vim.api.nvim_create_user_command("FixQuick", fix_quick, {})
-vim.keymap.set({"n","i"}, "]q", vim.cmd.cnext)
-vim.keymap.set({"n","i"}, "[q", vim.cmd.cprev)
+vim.keymap.set({"n","i"}, "]q", function()
+    vim.cmd.cnext()
+    vim.cmd.normal("zz")
+end)
+vim.keymap.set({"n","i"}, "[q", function()
+    vim.cmd.cprev()
+    vim.cmd.normal("zz")
+end)
 vim.keymap.set({"n","i"}, "<A-CR>", fix_quick)
 
 -- use :cexpr [] to clear the quickfix list
