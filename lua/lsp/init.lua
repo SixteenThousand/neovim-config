0248ca1c6891b2a62ebea7a976ed10f64e466835
Thomas Lindop <thomas16256@gmail.com>
2025-02-03 17:29:08 +0000

    >> remove setups of uninstalled LSPs

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,31 +2,13 @@ local lspconf = require("lspconfig")
 require("lsp.keymaps")
 
 
-lspconf.pylsp.setup{
-	settings = {
-		pylsp = {
-			plugins = {
-				autopep8 = {
-					enabled = false,
-				},
-				pycodestyle = {
-					enabled = false,
-				},
-			},
-		},
-	},
-}
 lspconf.pyright.setup{}
-
 lspconf.rust_analyzer.setup{
     cmd = {"rust-analyzer"},
 }
-
-lspconf.texlab.setup{}
 lspconf.gopls.setup{}
 lspconf.cssls.setup{}
-lspconf.sqls.setup{}
 lspconf.ts_ls.setup{}
 lspconf.clangd.setup{}
 lspconf.jdtls.setup{}
-lspconf.csharp_ls.setup{}
+-- lspconf.csharp_ls.setup{}
