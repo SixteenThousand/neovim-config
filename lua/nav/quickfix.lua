a23d5941e9c16d3ca261e4ed1e2e61948e1bb456
Thomas Lindop <thomas16256@gmail.com>
2025-02-20 18:41:28 +0000

    >> add location list key bindings > remove quickfix key bindings from insert mode

--- lua/nav/quickfix.lua
+++ lua/nav/quickfix.lua
@@ -4,14 +4,24 @@ local fix_quick = function()
 end
 
 vim.api.nvim_create_user_command("FixQuick", fix_quick, {})
-vim.keymap.set({"n","i"}, "]q", function()
+vim.keymap.set("n", "]q", function()
     vim.cmd.cnext()
     vim.cmd.normal("zz")
 end)
-vim.keymap.set({"n","i"}, "[q", function()
+vim.keymap.set("n", "[q", function()
     vim.cmd.cprev()
     vim.cmd.normal("zz")
 end)
 vim.keymap.set({"n","i"}, "<A-CR>", fix_quick)
 
 -- use :cexpr [] to clear the quickfix list
+
+--- location list
+vim.keymap.set("n", "]l", function()
+    vim.cmd.lnext()
+    vim.cmd.normal("zz")
+end)
+vim.keymap.set("n", "[l", function()
+    vim.cmd.lprev()
+    vim.cmd.normal("zz")
+end)
