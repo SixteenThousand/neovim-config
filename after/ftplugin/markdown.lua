aaf10df8647ea0f923026322346da32a16253fc4
Thomas Lindop <thomas16256@gmail.com>
2025-02-22 22:48:16 +0000

    >> add key bindings for new markdown plugin

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-CR>", "<Cmd>MDListItemBelow<CR>")
+vim.keymap.set("n", "<C-S-CR>", "<Cmd>MDResetListNumbering<CR>")
+vim.keymap.set("i", "<C-CR>", "<Esc><Cmd>MDListItemBelow<CR>")
+vim.keymap.set("i", "<C-S-CR>", "<Esc><Cmd>MDResetListNumbering<CR>")
